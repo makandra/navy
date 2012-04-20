b3879020f78bbb643e4fdeed82cd42c62063927a
Tobias Kraze <tobias@kraze.eu>
2012-04-20 14:11:45 +0200

    update cucumber steps to be work on a wider range of capybara versions

--- assets/cucumber/navy_steps.rb
+++ assets/cucumber/navy_steps.rb
@@ -1,4 +1,4 @@
-When /^I open the "([^"]*)" section$/ do |all_labels|
+When /^I open the "([^"]*)" (?:section|tab)$/ do |all_labels|
   labels = all_labels.split('>').collect(&:squish)
   if Capybara.current_session.driver.is_a? Capybara::Driver::Selenium
     if labels.size == 2
@@ -9,30 +9,28 @@ When /^I open the "([^"]*)" section$/ do |all_labels|
     opens = nil
     section = nil
     labels.each_with_index do |label, index|
-      scope = ".navy-level-#{index + 1}"
+      scope = "div[data-navy-navigation-level='#{index + 1}']"
       if opens
-        scope << "[data-opened-by='#{opens}']"
+        scope << "[data-navy-opened-by='#{opens}']"
       end
-      with_scope(scope) do
-        section = find_link(label)
-        all_labels.should(simple_matcher('a reachable section') { |given, matcher| section.present? })
-        opens = section["data-opens"]
-      end
+      scope << ' a'
+      page.should have_css(scope, :text => label)
+      section = find(scope, :text => label)
+      opens = section["data-navy-opens"]
     end
     section.click
   end
 end
 
-When /^I expand the "([^"]*)" section$/ do |label|
+When /^I expand the "([^"]*)" (?:section|tab)$/ do |label|
   page.execute_script("$('.navy-level-1 .navy-section:contains(\"#{label}\") .navy-section-expander').click()")
 end
 
-Then /^there should( not)? be a "([^"]*)" tab$/ do |negate, label|
-  expectation = negate ? "should not" : "should"
-  Then "I #{expectation} see \"#{label}\" within \".navy-navigation\""
+Then /^there should( not)? be a "([^"]*)" (?:section|tab)$/ do |negate, label|
+  expectation = negate ? "should_not" : "should"
+  page.send(expectation, have_css(".navy-navigation", :text => label))
 end
 
-Then /^I should be in the "([^"]*)" section$/ do |label|
-  Then "I should see \"#{label}\" within \".navy-navigation .navy-active\""
+Then /^I should be in the "([^"]*)" (?:section|tab)$/ do |label|
+  page.should have_css(".navy-navigation .navy-active", :text => label)
 end
-
